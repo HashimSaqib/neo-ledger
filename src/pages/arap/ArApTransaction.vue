9ea6bd623880d3b4b387d8d8ee23eabd0575b526
Hashim Saqib <hashim1saqib@gmail.com>
2026-02-23 10:45:20 +0100

    remove line tax recalculation

--- src/pages/arap/ArApTransaction.vue
+++ src/pages/arap/ArApTransaction.vue
@@ -1124,11 +1124,6 @@ const calculateTaxes = () => {
   if (!lineTax.value && preserveApiTaxes.value) return;
 
   if (lineTax.value) {
-    lines.value.forEach((line) => {
-      if (line.taxAccount) {
-        line.taxAmount = computeLineTaxAmount(line);
-      }
-    });
     const taxAgg = {};
     lines.value.forEach((line) => {
       if (line.taxAccount && line.taxAmount) {
